26b667c61a87819d1dceb736884d7839d47a3dca
VanshWebDev <vanshvanshkumar39@gmail.com>
2026-02-21 16:40:10 +0530

    OK

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import cors from "cors";
 import serverless from "serverless-http";
-import { addUserToSheet } from "../src/googleSheet";
+import { addUserToSheet } from "../src/googleSheet.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => res.send("API running"));
-app.get("/test", (req, res) => res.send("working"));
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.get("/test", (req, res) => {
+  res.send("working");
+});
 
 app.post("/submit", async (req, res) => {
   try {
