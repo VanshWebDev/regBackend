11c4d0ac7c1b683b52344da208d0a40a945c7f73
VanshWebDev <vanshvanshkumar39@gmail.com>
2026-02-21 11:49:39 +0530

    initial commit4

--- api/index.ts
+++ api/index.ts
@@ -1,31 +1,30 @@
 import express from "express";
-import { addUserToSheet } from "../src/services/googleSheet";
-import bodyParser from "body-parser";
 import cors from "cors";
+import serverless from "serverless-http";
+import { addUserToSheet } from "../src/services/googleSheet";
 
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/test", async (req, res) => {
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.get("/test", (req, res) => {
   res.send("working");
 });
 
 app.post("/submit", async (req, res) => {
   try {
-    console.log("body", req.body);
     const { name, email, phone } = req.body;
-
     await addUserToSheet(name, email, phone);
-
     res.json({ message: "Saved to sheet" });
-  } catch (error) {
-    console.log(error);
+  } catch {
     res.status(500).json({ message: "Error saving data" });
   }
 });
 
-export default app;
+export const handler = serverless(app);
+export default handler;
